211dccc6701651012a19258fe32770d051f48868
yrlrussell <yraniceta.student@ua.edu.ph>
2023-08-09 01:15:01 +0800

    first commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>HI I'M YRL RUSSELL ANICETA A 3RD YEAR COMPUTER ENGINEERING</p>
+        <p>
+          
+        </p>
+      </section>
+    </Layout>
+  );
+}
