46ceba1cf0fe2fa6fd6487a157f60ccfe0fbd122
yrlrussell <yraniceta.student@ua.edu.ph>
2023-08-24 17:04:17 +0800

    Third Comms

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <p>My impression of my first month as a CPE 3rd Year Student</p>
         <p>The first month as a 3rd year student is very exciting and
        challenging. Exciting because they introduced to us the fundamental
-        concepts like digital logic and etc. <p>
+        concepts like digital logic and etc. Im Glad because our Prof was teaching us now very well. <p>
 
           <p>I know now  how to update a website </p>
           <p>We are Trying to build a sample website using: <a href = "https://nextjs.org/learn"> our next js tutorial</a> </p>
